7e3f8c3f35497ed82f949a9107546d844b6c85ee
thollarkings <thollarkings.ca@gmail.com>
2025-06-18 11:53:29 +0100

    made some changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/receipt-reimagined-watermark/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" ? componentTagger() : null,
   ].filter(Boolean),
   resolve: {
     alias: {
